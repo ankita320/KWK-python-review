93f16167d78db872a007b0b8355ee2b3743bb4d5
Ankita Saha <76600603+ankita320@users.noreply.github.com>
2022-01-13 20:01:58 -0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -2,10 +2,13 @@
 python review for data science from KWK
 
 print ('hello kode with klossy world')
+
 lucky_number = 2 * (45 - 2**2) - (12 / 2) + 1
 print(lucky_number)
+
 my_string = "Karlie Kloss"
 my_str = 'adc'
 print(my_str)
+
 scholar_hometowns["Taylor"] = "Wyomissing"
 print(scholar_hometowns)
